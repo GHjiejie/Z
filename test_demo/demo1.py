bea17817d11a9048e2da3479462b4cece3451366
zhengjie <3426571530@qq.com>
2026-08-14 16:42:27 +0800

    Add chat model and tools for arithmetic operations; update .gitignore

--- test_demo/demo1.py
+++ test_demo/demo1.py
@@ -1,6 +1,6 @@
 from langchain.tools import tool
 
-from chat import chat_model
+from chat_models.chat import chat_model
 
 
 # 定义加法工具
@@ -24,12 +24,20 @@ def multiply(a: int, b: int) -> int:
     return a * b
 
 
+@tool
+def get_wather(city: str) -> str:
+    """Get the weather for a given city."""
+    # 这里可以调用一个天气API来获取天气信息
+    return f"The weather in {city} is sunny."
+
+
+tools = [add, subtract, multiply, get_wather]
+
 # 然后接下来的思路就是在接受用户输入的时候，怎么判断要调用你一个工具
 
 # 将工具绑定到模型上
-tools = [add, subtract, multiply]
-
 model_with_tools = chat_model.bind_tools(tools)
 
-response = model_with_tools.invoke("告诉我1+7等于多少？")
+
+response = model_with_tools.invoke("深圳的天气怎么样？")
 print(response)
