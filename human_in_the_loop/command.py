494901f4a623419be1deafc580b2fb51584d76ff
zhengjie <3426571530@qq.com>
2026-08-24 16:40:22 +0800

    feat:use command to finish the interrupt job

---
+++ human_in_the_loop/command.py
@@ -0,0 +1,91 @@
+from typing import Annotated, TypedDict
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.runnables import RunnableConfig
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph import END, START, StateGraph
+from langgraph.graph.message import add_messages
+from langgraph.types import Command, interrupt
+
+from chat_models.chat import chat_model
+
+dp_path = "./human_in_the_loop/human_in_the_loop.sqlite"
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+def llm_call(state: ChatState):
+    response = chat_model.invoke(state["messages"])
+    return {"messages": [response]}
+
+
+def approve_node(state: ChatState):
+
+    approved = interrupt(
+        "Please approve the model answer the question. Type 'yes' to approve or 'no' to reject."
+    )
+
+    if approved.lower() == "yes":
+        print("Response approved.")
+        return Command(goto="llm_call")
+
+    else:
+        print("Response rejected. Please provide feedback.")
+        return Command(goto="END")
+
+
+builder = StateGraph(ChatState)
+builder.add_node("llm_call", llm_call)
+builder.add_node("approve_node", approve_node)
+builder.add_edge(START, "approve_node")
+builder.add_edge("llm_call", END)
+
+
+config: RunnableConfig = {"configurable": {"thread_id": "session_human_in_loop"}}
+
+
+def main():
+    with SqliteSaver.from_conn_string(dp_path) as checkpointer:
+        graph = builder.compile(checkpointer=checkpointer)
+
+        while True:
+            state = graph.get_state(config=config)
+
+            stream_input: ChatState | Command
+
+            if state.next:
+                interrupt_msg = state.tasks[0].interrupts[0].value
+
+                human_feedback = input(f"\n[System] {interrupt_msg}\napproval: ")
+
+                stream_input = Command(resume=human_feedback)
+
+            else:
+                user_input = input("\nuser: ")
+                if not user_input.strip():
+                    continue
+                stream_input = ChatState(messages=[HumanMessage(content=user_input)])
+
+            for part in graph.stream(
+                stream_input,
+                config=config,
+                stream_mode="messages",
+                version="v2",
+            ):
+                if part["type"] != "messages":
+                    continue
+
+                message_chunk, metadata = part["data"]
+
+                if metadata.get("langgraph_node") not in ["llm_call", "approve_node"]:
+                    continue
+
+                text = str(message_chunk.text)
+                if text:
+                    print(text, end="", flush=True)
+
+
+if __name__ == "__main__":
+    main()
